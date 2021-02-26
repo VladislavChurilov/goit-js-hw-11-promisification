82a75772f597aacc8f43eb29f4cc41ab8423a666
Vladislav Churilov <88vvch88@gmail.com>
2021-02-27 01:20:45 +0200

    hw11

---
+++ goit-js-hw-11-promisification/src/task-2.js
@@ -0,0 +1,22 @@
+
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+  const toggleUserState = (allUsers, userName) => {
+    return new Promise(resolve => {
+    resolve(allUsers, userName);    
+    });    
+  };
+  
+  const logger = updatedUsers => console.table(updatedUsers);
+ 
+  /*
+   * Должно работать так
+   */
+  toggleUserState(users, 'Mango').then(logger);
+  toggleUserState(users, 'Lux').then(logger);
